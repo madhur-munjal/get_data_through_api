511700643406e9fd2a62fa73cb6db709341aa297
Madhur Munjal <madhur.munjal@yahoo.in>
2025-07-30 14:26:06 +0530

    update user models

--- src/models/user.py
+++ src/models/user.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Text
 from src.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
+    username = Column(Text, nullable=True) #String, unique=True, index=True)
+    password = Column(Text, nullable=True) #String)
